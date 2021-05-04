b5257efb55725782bcafc9425fcb3340d467165f
Pengfang Liu <lpf_mart@163.com>
2021-05-04 23:56:33 +0800

    添加项目文件。

---
+++ NewCSharp9Feature/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NewCSharp9Feature
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
